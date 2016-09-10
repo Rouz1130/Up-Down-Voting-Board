c109954db74b12c5a61bf0b1d85d179590efc7f6
rouz majlessi <rmajlessi@gmail.com>
2016-09-10 16:55:28 -0700

    populate both question and model file and fix issues

--- app/models/answer.js
+++ app/models/answer.js
@@ -1,5 +1,9 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
+  content: DS.attr(),
+  author: DS.attr(),
+  votes: DS.attr(),
+  question: DS.belongsTo('question', {async:true})
 
 });
