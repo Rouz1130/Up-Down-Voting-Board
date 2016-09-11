8b57f49cf1b1a1d7c03c5927a8d95ce90271df4b
rouz majlessi <rmajlessi@gmail.com>
2016-09-10 17:57:09 -0700

    add actions to delete and edit answer

--- app/components/answer-tile.js
+++ app/components/answer-tile.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    editAnswer(answer, params) {
+      this.sendAction('editAnswer', answer, params);
+    },
+    deleteAnswer(answer) {
+      this.sendAction('deleteAnswer', answer);
+    }
+  }
 });
