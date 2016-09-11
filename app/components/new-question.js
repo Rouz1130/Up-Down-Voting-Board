cd72dd8d64bd8500370c8c9c4ed7cd861bac5fc2
rouz majlessi <rmajlessi@gmail.com>
2016-09-10 17:14:32 -0700

    change question file add model instead of name aslo add save method

--- app/components/new-question.js
+++ app/components/new-question.js
@@ -1,4 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  addNewQuestion: false,
+  actions: {
+    questionFormShow() {
+      this.set('addNewQuestion', true);
+    },
+
+    questionSave1() {
+      var params = {
+        text: this.get('text') ? this.get('text') : "",
+        author: this.get('author') ? this.get('author') : "",
+        notes: this.get('notes') ? this.get('notes') : "",
+        votes: 0,
+      };
+      this.set('addNewQuestion', false),
+      this.sendAction('questionSave2', params);
+    }
+  }
 });
--- app/routes/index.js
+++ app/routes/index.js
@@ -7,4 +7,11 @@ export default Ember.Route.extend({
       answers: this.store.findAll('answer')
     });
   },
+  actions: {
+    questionSave3(params) {
+      var newQuestion = this.store.createRecord('question', params);
+      newQuestion.save();
+      this.transitionTo('index');
+    }
+  }
 });
