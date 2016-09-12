d5a569dc63a528a61004da613d1ac5e974a1d766
rouz majlessi <rmajlessi@gmail.com>
2016-09-12 15:18:28 -0700

    populate edit answer

--- app/components/edit-answer.js
+++ app/components/edit-answer.js
@@ -1,4 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  editAnswerForm: false,
+  actions: {
+    editAnswerForm() {
+      this.set('editAnswerForm', true);
+    },
+    editAnswer(answer) {
+      var params = {
+        author:this.get('author'),
+        content:this.get('content'),
+        question:this.get('question')
+      };
+      this.set('editAnswerForm', false);
+      this.sendAction('editAnswer', answer, params);
+    }
+  }
 });
