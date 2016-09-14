371123d26202fb0250449ea9ef1d70356a1a1cd8
rouz majlessi <rmajlessi@gmail.com>
2016-09-13 22:47:04 -0700

    populate answer display component file

--- app/components/answer-display.js
+++ app/components/answer-display.js
@@ -1,4 +1,22 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+    sortBy: ['votes:desc'],
+    sortedAnswers: Ember.computed.sort('model.answers', 'sortBy'),
+
+    actions: {
+      upvote(answer){
+        this.sendAction('upvote', answer);
+      },
+      downvote(answer){
+        this.sendAction('downvote', answer);
+      },
+      editAnswer(answer, params){
+        console.log("answers-display");
+        this.sendAction('editAnswer', answer, params);
+      },
+      deleteAnswer(answer){
+        this.sendAction('deleteAnswer', answer);
+      },
+    }
 });
